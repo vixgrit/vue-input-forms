e48f4495246f4908d84ad0271c79d8285403c987
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-07 01:34:05 -0400

    use radio buttons

--- src/App.vue
+++ src/App.vue
@@ -48,10 +48,10 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                 <label for="male">
-                    <input type="radio" id="male" value="Male"> Male
+                    <input type="radio" id="male" value="Male" v-model="gender"> Male
                 </label>
                 <label for="female">
-                    <input type="radio" id="female" value="Female"> Female
+                    <input type="radio" id="female" value="Female" v-model="gender"> Female
                 </label>
             </div>
         </div>
@@ -87,7 +87,7 @@
                     <ul>
                       <li v-for="item in sendMail">{{ item }}</li>
                     </ul>
-                    <p>Gender:</p>
+                    <p>Gender: {{ gender }}</p>
                     <p>Priority:</p>
                     <p>Switched:</p>
                 </div>
@@ -107,7 +107,8 @@
           age: 27
         },
         message: 'A new text message',
-        sendMail: []
+        sendMail: [],
+        gender: 'Male'
       }
     }
   }
